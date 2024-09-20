b8c2a885c092235746a4542086e40541a8f96d25
mauwqq <mauw0@protonmail.com>
2024-09-20 20:29:59 -0300

    Funciones sin usar eliminadas, mejor organizacion de funciones, modulos importados de forma completa y typehints con typing

--- modulos/menu.py
+++ modulos/menu.py
@@ -1,14 +1,8 @@
 """Modulo para imprimir el menu del gestor."""
 
-from modulos.tablas_del_sistema import imprimir_tabla, cargar_data
-from modulos.reservas import (
-    registrar_reserva,
-    consultar_reserva,
-    anular_reserva,
-    registrar_check_in,
-    registrar_check_out,
-)
-
+from modulos import tablas_del_sistema
+from modulos import reservas
+from typing import List, Dict
 # from modulos.facturacion import emitir_facturas
 import json
 import os
@@ -19,7 +13,7 @@ def clear() -> None:
     os.system("cls" if os.name == "nt" else "clear")
 
 
-def pedir_numero(opciones: dict[dict]) -> int:
+def pedir_numero(opciones: Dict[str,str]) -> int:
     """Solicita al usuario seleccionar un número de opción.
 
     Pre: Recibe un diccionario "opciones" que contiene las opciones válidas como
@@ -29,14 +23,6 @@ def pedir_numero(opciones: dict[dict]) -> int:
           seleccionada por el usuario.
 
     """
-    if name == "nt":
-        _ = system("cls")
-    else:
-        _ = system("clear")
-    return None
-
-
-def pedir_numero(opciones: dict[str]) -> int:
     while True:
         try:
             n = int(input("Seleccione una opción: "))
@@ -47,7 +33,7 @@ def pedir_numero(opciones: dict[str]) -> int:
             print("Debe ingresar un número.")
 
 
-def mostrar_menu_y_pedir_numero(nombre_menu: str, config: dict[dict]) -> int:
+def mostrar_menu_y_pedir_numero(nombre_menu: str, config: Dict[str, Dict[str, str]]) -> int:
     """Muestra el menú correspondiente y solicita una opción al usuario.
 
     Pre: "nombre_menu" es una cadena que representa el nombre de un menú
@@ -58,7 +44,6 @@ def mostrar_menu_y_pedir_numero(nombre_menu: str, config: dict[dict]) -> int:
           seleccionada en el menú.
 
     """
-def mostrar_menu(nombre_menu: str, config: dict[str]) -> int:
     clear()
     opciones = config[nombre_menu]
     for clave, valor in opciones.items():
@@ -69,7 +54,6 @@ def mostrar_menu(nombre_menu: str, config: dict[str]) -> int:
 
 def no_implementado() -> None:
     """Informa al usuario que una opción no está implementada."""
-
     print("Esta opción no está implementada aún.")
     print("Regresando al menú...")
     time.sleep(2)
@@ -84,10 +68,8 @@ def menu_principal() -> None:
           opción seleccionada por el usuario.
 
     """
-    config = cargar_configuracion("data/menu.json")
+    config = tablas_del_sistema.cargar_data("data/menu.json")
     op = mostrar_menu_y_pedir_numero("menu_principal", config)
-def menu_principal(config: dict[str]) -> None:
-    op = mostrar_menu("menu_principal", config)
     match op:
         case 1:
             menu_tablero(config)
@@ -107,7 +89,7 @@ def menu_principal(config: dict[str]) -> None:
     return None
 
 
-def menu_tablero(config: dict[dict]) -> None:
+def menu_tablero(config: Dict[str, Dict[str, str]]) -> None:
     """Muestra el menú del tablero y ejecuta la opción seleccionada.
 
     Pre: "config" es un diccionario que contiene en un diccionario el menú del
@@ -123,24 +105,10 @@ def menu_tablero(config: dict[dict]) -> None:
             no_implementado()
         case 0:
             menu_principal()
-            menu_principal(config)
     return None
 
 
-def menu_tablero(config: dict[str]) -> None:
-    op = mostrar_menu("menu_tablero", config)
-    match op:
-        case 1:
-            pass
-        case 0:
-            menu_principal(config)
-        case _:
-            print("Opción inválida. Regresando al menú de Tablero.")
-            menu_tablero(config)
-    return None
-
-
-def menu_reservas(config: dict[dict]) -> None:
+def menu_reservas(config: Dict[str, Dict[str, str]]) -> None:
     """Muestra el menú de reservas y ejecuta la opción seleccionada.
 
     Pre: "config" es un diccionario que contiene el menú de reservas.
@@ -150,48 +118,33 @@ def menu_reservas(config: dict[dict]) -> None:
 
     """
     op = mostrar_menu_y_pedir_numero("menu_reservas", config)
-    config = cargar_data("data/reservas.json")
+    config = tablas_del_sistema.cargar_data("data/reservas.json")
     match op:
         case 1:
-            registrar_reserva(config)
+            reservas.registrar_reserva(config)
             if input("Presione 'Enter' para volver al menu.") == '':
                 menu_principal()
         case 2:
-            consultar_reserva(config)
+            reservas.consultar_reserva(config)
             if input("Presione 'Enter' para volver al menu.") == '':
                 menu_principal()
         case 3:
-            anular_reserva(config)
+            reservas.anular_reserva(config)
             if input("Presione 'Enter' para volver al menu.") == '':
                 menu_principal()
         case 4:
-            registrar_check_in(config)
+            reservas.registrar_check_in(config)
             if input("Presione 'Enter' para volver al menu.") == '':
                 menu_principal()
         case 5:
-            registrar_check_out(config)
+            reservas.registrar_check_out(config)
             if input("Presione 'Enter' para volver al menu.") == '':
                 menu_principal()
         case 0:
             menu_principal()
-def menu_reservas(config: dict[str]) -> None:
-    op = mostrar_menu("menu_reservas", config)
-    match op:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 0:
-            menu_principal(config)
-        case _:
-            print("Opción inválida. Regresando al menú de Reservas.")
-            menu_reservas(config)
-    return None
 
 
-def menu_consumo_frigobar(config: dict[dict]) -> None:
+def menu_consumo_frigobar(config: Dict[str, Dict[str, str]]) -> None:
     """Muestra el menú de consumo de frigobar y ejecuta la opción seleccionada.
 
     Pre: "config" es un diccionario que contiene el menú de consumo
@@ -208,22 +161,9 @@ def menu_consumo_frigobar(config: dict[dict]) -> None:
             no_implementado()
         case 0:
             menu_principal()
-def menu_consumo_frigobar(config: dict[str]) -> None:
-    op = mostrar_menu("menu_consumo_frigobar", config)
-    match op:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 0:
-            menu_principal(config)
-        case _:
-            print("Opción inválida. Regresando al menú de Consumo de Frigobar.")
-            menu_consumo_frigobar(config)
-    return None
 
 
-def menu_facturacion(config: dict[dict]) -> None:
+def menu_facturacion(config: Dict[str, Dict[str, str]]) -> None:
     """Muestra el menú de facturación y ejecuta la opción seleccionada.
 
     Pre: "config" es un diccionario que contiene el menú de facturación.
@@ -243,24 +183,9 @@ def menu_facturacion(config: dict[dict]) -> None:
             no_implementado()
         case 0:
             menu_principal()
-def menu_facturacion(config: dict[str]) -> None:
-    op = mostrar_menu("menu_facturacion", config)
-    match op:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 0:
-            menu_principal(config)
-        case _:
-            print("Opción inválida. Regresando al menú de Facturación.")
-            menu_facturacion(config)
-    return None
 
 
-def menu_tablas_del_sistema(config: dict[dict]) -> None:
+def menu_tablas_del_sistema(config: Dict[str, Dict[str, str]]) -> None:
     """Muestra el menú de tablas del sistema y ejecuta la opción seleccionada.
 
     Pre: "config" es un diccionario que contiene el menú de tablas del sistema.
@@ -272,15 +197,15 @@ def menu_tablas_del_sistema(config: dict[dict]) -> None:
     op = mostrar_menu_y_pedir_numero("menu_tablas_del_sistema", config)
     match op:
         case 1:
-            imprimir_tabla("data/clientes.csv")
+            tablas_del_sistema.imprimir_tabla("data/clientes.csv")
             if input("Presione 'Enter' para volver.") == "":
                 menu_principal()
         case 2:
-            imprimir_tabla("data/habitaciones.csv")
+            tablas_del_sistema.imprimir_tabla("data/habitaciones.csv")
             if input("Presione 'Enter' para volver.") == "":
                 menu_principal()
         case 3:
-            imprimir_tabla("data/productos.csv")
+            tablas_del_sistema.imprimir_tabla("data/productos.csv")
             if input("Presione 'Enter' para volver.") == "":
                 menu_principal()
         case 4:
@@ -289,27 +214,7 @@ def menu_tablas_del_sistema(config: dict[dict]) -> None:
             no_implementado()
         case 0:
             menu_principal()
-def menu_tablas_del_sistema(config: dict[str]) -> None:
-    op = mostrar_menu("menu_tablas_del_sistema", config)
-    match op:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass
-        case 5:
-            pass
-        case 0:
-            menu_principal(config)
-        case _:
-            print("Opción inválida. Regresando al menú de Tablas del Sistema.")
-            menu_tablas_del_sistema(config)
-    return None
 
 
 if __name__ == "__main__":
-   config = cargar_configuracion('menu.json')
-   menu_principal(config)
+   menu_principal()
